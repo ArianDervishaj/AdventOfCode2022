bb2e3b2dc84bdc2c22b2e090a5e77d1db26e2722
ArianDervishaj <ArianDervishaj@icloud.com>
2022-12-05 18:37:06 +0100

    debut du jour 2

--- Day-02/day02.js
+++ Day-02/day02.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+/*const fs = require('fs');
 
 //take the data from the day01.txt file
 fs.readFile('./day02.txt', 'utf-8', (err, data) => {
@@ -6,8 +6,26 @@ fs.readFile('./day02.txt', 'utf-8', (err, data) => {
 
     console.log(data);
 } )
+*/
+const WIN = 6;
+const DRAW = 3;
+const LOOSE = 0;
 
 
+
+const testInput = `
+A Y
+B X
+C Z
+`;
+
+const strat = 
+    testInput
+        .trim()
+        .split("\n");
+
+const scoreByGame = strat.map()
+console.log(strat);
 
 
 
