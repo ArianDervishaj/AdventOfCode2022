e183ac167b800be92f08a38cb69970bdb1c41807
ArianDervishaj <ArianDervishaj@icloud.com>
2022-12-06 23:33:26 +0100

    day 3

--- Day-03/day03.js
+++ Day-03/day03.js
@@ -1,14 +1,35 @@
 const fs = require('fs');
 
-//take the data from the day02.txt file
-fs.readFile('./day02.txt', 'utf-8', (err, data) => {
-    if (err) throw err;
-
-    //Sanitize the data and make it usable.
-    const strat = 
-    data
-        .trim()
-        .split("\n");
+function splitArray(array){
+    
+    const nbOfPart = 2;
+    let splitedArray = [[],[]];
 
     
+    
+}
+
+//take the data from the day02.txt file
+fs.readFile('./day03.txt', 'utf-8', (err, data) => {
+    if (err) throw err;
+
+    const dataSample = `
+    vJrwpWtwJgWrhcsFMMfFFhFp
+    jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+    PmmdzqPrVvPwwTWBwg
+    wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+    ttgJtRGJQctTZtZT
+    CrZsJsPPZsGzwwsLwLmpwMDw
+    `;
+
+    //Sanitize the data and make it usable.
+    const rucksacks = 
+    dataSample
+        .trim()
+        .split("\n");
+        
+        
+    rucksacks.map(rucksack => {
+        console.log(rucksack);
+    })
 } )
