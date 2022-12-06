bc1dfed74606727214f7f41326df4e02d65ea87c
ArianDervishaj <ArianDervishaj@icloud.com>
2022-12-07 00:04:39 +0100

    function to split rucksack in 2

--- Day-03/day03.js
+++ Day-03/day03.js
@@ -5,21 +5,29 @@ function splitArray(array){
     const nbOfPart = 2;
     let splitedArray = [[],[]];
 
-    
+    for(let left = 0; left < array.length / nbOfPart; left++){
+        splitedArray[0].push(array[left]);
+    }
+
+    for(let right = array.length / nbOfPart; right < array.length; right++){
+        splitedArray[1].push(array[right])
+    }
+
+    return splitedArray
     
 }
 
-//take the data from the day02.txt file
+//take the data from the day03.txt file
 fs.readFile('./day03.txt', 'utf-8', (err, data) => {
     if (err) throw err;
 
     const dataSample = `
     vJrwpWtwJgWrhcsFMMfFFhFp
-    jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
-    PmmdzqPrVvPwwTWBwg
-    wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
-    ttgJtRGJQctTZtZT
-    CrZsJsPPZsGzwwsLwLmpwMDw
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
     `;
 
     //Sanitize the data and make it usable.
@@ -30,6 +38,12 @@ fs.readFile('./day03.txt', 'utf-8', (err, data) => {
         
         
     rucksacks.map(rucksack => {
-        console.log(rucksack);
+        const splitedRucksack = splitArray(rucksack);
+        const leftRucksack = splitedRucksack[0];
+        const rightRucksack = splitedRucksack[1];
+
+
+        console.log(leftRucksack + " & " + rightRucksack);
+        console.log(rucksack.length + " " + leftRucksack.length + " " + rightRucksack.length)
     })
 } )
