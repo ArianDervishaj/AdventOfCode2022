24400ac0a414e91140fef12a8d90273699ddd95d
ArianDervishaj <ArianDervishaj@icloud.com>
2022-12-07 02:31:09 +0100

    pas finis

--- Day-03/day03.js
+++ Day-03/day03.js
@@ -16,7 +16,23 @@ function splitArray(array){
     return splitedArray
     
 }
+function sumOfPriorites(array){
+    const asciiCodeA = 65;
+    const asciiCodea = 97;
+    let sum = 0;
 
+    for(let i = 0; i < array.length; i++){
+        if(array[i].charCodeAt(0) < 91 && array[i].charCodeAt(0) > 64 ){ //check if char is upper case
+            sum += (array[i].charCodeAt(0) - asciiCodeA + 1 + 26);
+        }
+        else{
+            sum += (array[i].charCodeAt(0) - asciiCodea + 1);
+        }
+
+    }
+
+    return sum;
+}
 //take the data from the day03.txt file
 fs.readFile('./day03.txt', 'utf-8', (err, data) => {
     if (err) throw err;
@@ -28,34 +44,50 @@ PmmdzqPrVvPwwTWBwg
 wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
 ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw
+DvScpDDVfBPqVcSbDpbfHpqwCWGJCwCJhvFdFFwlMGJWCG
+fhHPbhZSpDbDTmsTZTjRzzQz
+QbVQvNrrdFcbcMvvdNrcGrrczPnPplPnfnpzwpgmlflRVwwP
+jBHLRqZWtLLqWDhBLshBCLsHgpfftfPmmpfpwnwfwJgfpfPn
+WCRhsLDsDLBChTLWHChFTTMdGTQGvGQcMQNGMd
+DvScpDDVfBPqVcSbDpbfHpqwCWGJCwCJhvFdFFwlMGJWCG
+fhHPbhZSpDbDTmsTZTjRzzQz
+QbVQvNrrdFcbcMvvdNrcGrrczPnPplPnfnpzwpgmlflRVwwP
+jBHLRqZWtLLqWDhBLshBCLsHgpfftfPmmpfpwnwfwJgfpfPn
+WCRhsLDsDLBChTLWHChFTTMdGTQGvGQcMQNGMd
+PFFTlhllffsNsscl
     `;
 
     //Sanitize the data and make it usable.
     const rucksacks = 
-    dataSample
+    data
         .trim()
         .split("\n");
         
+    console.log(rucksacks)
     let bothArray = [];
     rucksacks.map(rucksack => {
         const splitedRucksack = splitArray(rucksack);
         const leftRucksack = splitedRucksack[0];
         const rightRucksack = splitedRucksack[1];
-
-
+        let bothArrayDeletable = [];
 
         for(let left = 0; left < leftRucksack.length; left++){
             for(let right = 0; right < rightRucksack.length; right++){
 
                 if(leftRucksack[left] === rightRucksack[right]){
 
-                    if(!bothArray.includes(leftRucksack[left])) //if the letter isn't already in the array pushes it in the array
-                        bothArray.push(leftRucksack[left]);
+                    if(!bothArrayDeletable.includes(leftRucksack[left])){ //if the letter isn't already in the array pushes it in the array
+                        bothArrayDeletable.push(leftRucksack[left]);
+                        bothArray.push(leftRucksack[left])
+                    }
+
                 }
 
             }
         }
 
-    })
+
+    } )
     console.log(bothArray);
-} )
+    console.log(sumOfPriorites(bothArray));
+})
